09a1df31f96948c76bf1f222ae23bf0f7b7eb3b7
Gan Luanqing <ylluanq@163.com>
2021-06-04 10:31:30 +0800

    dev2 功能开发完成

---
+++ spring-boot/src/main/java/com/lantin/springboot/controller/DevController1.java
@@ -0,0 +1,18 @@
+package com.lantin.springboot.controller;
+
+/**
+ * Created on 2021/06/04/10:30 周五
+ *
+ * @author Lantin
+ */
+public class DevController1 {
+
+
+
+
+    public void dev2(){
+
+
+        System.out.println("dev222222开发完成了");
+    }
+}
